908f8917675d8e38eb9536e7f64ce856e3c11e06
ChangAn1988 <alstonbrett@gmail.com>
2016-12-22 05:27:38 +0800

    Initial Commit

---
+++ TryToGoogleAdMob/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TryToGoogleAdMob
+//
+//  Created by 陳昶安 on 2016/12/22.
+//  Copyright © 2016年 ANAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
